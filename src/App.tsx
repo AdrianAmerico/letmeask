aa76cee9a476f67d76b1d0c892c8b7e418b7ccbe
Adrian Américo <1fnadrian1@gmail.com>
2021-06-24 09:12:01 -0300

    Rotas finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { NewRoom } from "./pages/NewRoom";
 import { Home } from "./pages/Home";
+import { AuthContextProvider } from "./contexts/AuthContextProvider";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/rooms/new" component={NewRoom} />
-      </Switch>
+      <AuthContextProvider>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/rooms/new" component={NewRoom} />
+        </Switch>
+      </AuthContextProvider>
     </BrowserRouter>
   );
-}
+};
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,23 @@
 import { useHistory } from "react-router-dom";
-
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
 import "../styles/auth.scss";
 
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
 
-  const navigateToNewRoom = () => {
+  const handleCreateRoom = async () => {
+    if (!user) {
+      await signInWithGoogle();
+    }
+
     history.push("rooms/new");
   };
+
   return (
     <div id="page-auth">
       <aside>
@@ -22,7 +28,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="letmeask" />
-          <button className="create-room" onClick={navigateToNewRoom}>
+          <button className="create-room" onClick={handleCreateRoom}>
             <img src={googleIconImg} alt="Logo da google" />
             Crie sua sala com o Google
           </button>
